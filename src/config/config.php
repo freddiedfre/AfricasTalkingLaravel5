a25df21834767a5bea8145b612229d426e63006a
Fredrick A <fredrickanyera@gmail.com>
2016-02-19 21:29:22 +0300

    Configuration file reset

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,6 @@
 <?php 
 return [
-    'username' => 'freddiedfre',
-    'api_key' => 'faa61d35690193cb3f11f6f9ef5cd9ff6ad74d7d3f561c10c2236ef9d00fe917',
+    'username' => 'Your Username here',
+    'api_key' => 'Your API key here',
     ];
 
